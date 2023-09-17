c3c5e5067543dab70dd6533a49207908e06e03c0
luansud <luanfmedeiros@hotmail.com>
2023-09-16 21:37:33 -0300

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please Type your Grade:");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90){letter="A";}
+        else if (grade >= 80){letter="B";}
+        else if (grade >= 70){letter="C";}
+        else if (grade >= 60){letter="D";}
+        else {letter="F";}
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade>=70){System.Console.WriteLine("Congratulations, You passed!");}
+        else {System.Console.WriteLine("I'm sorry, Better luck next time!");}
     }
 }
